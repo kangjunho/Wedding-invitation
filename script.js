31398cd6c7d059315e0edb417bc45997d3421243
real bounce back <31853781+kangjunho@users.noreply.github.com>
2025-03-21 21:24:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const flowerContainer = document.querySelector(".flower-container");
-
-    function createFlower() {
-        const flower = document.createElement("div");
-        flower.classList.add("flower");
-        flower.style.left = Math.random() * 100 + "vw"; // 랜덤 위치
-        flower.style.animationDuration = Math.random() * 5 + 3 + "s"; // 떨어지는 속도 조절
-        flowerContainer.appendChild(flower);
-
-        setTimeout(() => {
-            flower.remove();
-        }, 8000); // 애니메이션 후 제거
-    }
-
-    setInterval(createFlower, 500); // 꽃이 생성되는 간격
+    $("nav ul li a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    });
 });
