ccb51d32e8a6a3b441dfa02357ffb94468cf4412
real bounce back <31853781+kangjunho@users.noreply.github.com>
2025-03-21 21:54:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function scrollToSection(sectionId) {
-    $('html, body').animate({
-        scrollTop: $('#' + sectionId).offset().top
-    }, 800);
-}
+$(document).ready(function () {
+    $("#menu-toggle").click(function (event) {
+        event.preventDefault();
+        $("#sidebar").css("right", "0px");
+    });
+
+    $("#close-sidebar").click(function (event) {
+        event.preventDefault();
+        $("#sidebar").css("right", "-300px");
+    });
+
+    function scrollToSection(sectionId) {
+        $('html, body').animate({
+            scrollTop: $('#' + sectionId).offset().top
+        }, 800);
+    }
+});
